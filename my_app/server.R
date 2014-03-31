87f5e1707e68b0340aee3a8dae9a7128e1019ee5
j23414 <jenchang@iastate.edu>
2014-03-31 17:40:49 -0500

    added shiny examples, next is mpg

--- my_app/server.R
+++ my_app/server.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(datasets)
 
 shinyServer(function(input,output){
   
@@ -6,6 +7,26 @@ shinyServer(function(input,output){
     x <- faithful[,2] # Old faithful Geyser data
     bins<-seq(min(x),max(x),length.out=input$bins+1)
     hist(x,breaks=bins,col='darkgray',border='white')
+  })
+  
+  datasetInput<-reactive({
+    switch(input$dataset,
+           "rock"=rock,
+           "pressure"=pressure,
+           "cars"=cars)
+  })
+  
+  output$summary<-renderPrint({
+    dataset<-datasetInput()
+    summary(dataset)
+  })
+  
+  output$view<-renderTable({
+    head(datasetInput(),n=input$obs)
+  })
+  
+  output$caption<-renderText({
+    input$caption
   })
   
 })
--- my_app/ui.R
+++ my_app/ui.R
@@ -5,13 +5,19 @@ shinyUI(fluidPage(
   titlePanel("E coli Stress Effects"),
   
   sidebarLayout(
-    
+
     sidebarPanel(
-      sliderInput("bins","Number of bins:", min=1,max=50,value=30)
+      sliderInput("bins","Number of bins:", min=1,max=50,value=30),
+      textInput("caption","Caption","Data Summary"),
+      selectInput("dataset", "Choose a dataset:",choices=c("rock","pressure","cars")),
+      numericInput("obs","Number of observations to view:",10)
     ),
     
     mainPanel(
-      plotOutput("distPlot")
+      plotOutput("distPlot"),
+      h3(textOutput("caption",container=span)),
+      verbatimTextOutput("summary"),
+      tableOutput("view")
     )
   )
 ))
