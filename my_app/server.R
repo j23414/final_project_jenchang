918cf225f1422a0644d905a2648056a436fa046c
j23414 <jenchang@iastate.edu>
2014-04-25 12:53:23 -0500

    working very basic shiny app

--- my_app/server.R
+++ my_app/server.R
@@ -4,33 +4,38 @@ library(wordcloud)
 library(tm)
 
 g_c_d<-read.csv("../data/gen_cluster_des.txt", header=TRUE, stringsAsFactors=FALSE)
+c_mod<-subset(g_c_d, cluster=="magenta")
+
+# function to plot wordcloud for a module
+plot_mod_wordcloud<-function(mod){
+  current_mod<-subset(g_c_d, cluster==mod)
+  wc.input<-paste(current_mod$desc,collapse="")
+  
+  path_words<-Corpus(VectorSource(wc.input))
+  path_words <- tm_map(path_words,stripWhitespace)
+  path_words <- tm_map(path_words,tolower)
+  path_words <- tm_map(path_words,removeWords,stopwords("english"))
+  #path_words <- tm_map(path_words,stemDocument)
+  
+  par(mar=rep(2,4))
+  wordcloud(path_words,scale=c(5,0.5),max.words=100,random.order=FALSE,rot.per=0.35,use.r.layout=FALSE,colors=brewer.pal(8,"Dark2"))
+}
 
 shinyServer(function(input,output){
   output$out2<-renderText({
-    input$in2
+    input$in3
   })
   
   output$outplot<-renderPlot(function(){
-    #x<-rnorm(100,mean=50,sd=40)
-    #hist(x,breaks=input$in4,col='darkgray',border='white')
-    
-    # blue, turquoise, yellow, black, green, brown, red, pink, magenta, grey
-    input$in3="grey"
-    wc.mod<-subset(g_c_d,cluster=input$in3)
-    wc.text<-paste(wc.mod$gene_description,collapse="")
-    words<-Corpus(VectorSource(wc.text))
-    words <- tm_map(words,stripWhitespace)
-    words <- tm_map(words,tolower)
-    words <- tm_map(words,removeWords,stopwords("english"))
-    par(mar=rep(2,4))
-    wordcloud(words,scale=c(5,0.5),max.words=100,random.order=FALSE,rot.per=0.35,use.r.layout=FALSE,colors=brewer.pal(8,"Dark2"))
+    plot_mod_wordcloud(input$in3)
+  })
+  
+  output$view<-renderTable({
+    c_mod=subset(g_c_d, cluster==input$in3)
+    head(c_mod,n=input$in4)
   })
   
   output$summary<-renderPrint({
     summary(mtcars)
-  })
-  
-  output$view<-renderTable({
-    head(mtcars,n=input$in4)
   })
 })
--- my_app/ui.R
+++ my_app/ui.R
@@ -2,19 +2,19 @@ library(shiny)
 
 shinyUI(fluidPage(
   
-  titlePanel("Page Title"),
+  titlePanel("Gene Description Word Cloud"),
   
   sidebarLayout(
     
     sidebarPanel(
       
-      sliderInput("in1", "SliderName:",min=1,max=100,value=50),
-      textInput("in2","TextName:","default text"),
-      selectInput("in3","SelectName:",choices=c("magenta","blue","turquoise","green","pink","red","black","brown","grey"), "magenta"),
+      #sliderInput("in1", "SliderName:",min=1,max=100,value=50),
+      #textInput("in2","TextName:","default text"),
+      selectInput("in3","Select Gene Cluster:",choices=c("magenta","blue","turquoise","green","pink","red","black","brown","grey"), "magenta"),
       numericInput("in4","NumericName:",10),
-      radioButtons("in5","RadioName:",c("choice 1","choice2","choice3"),"choice2"),
-      checkboxInput("in6","CheckName:",TRUE),
-      br(), # extra empty line
+      #radioButtons("in5","RadioName:",c("choice 1","choice2","choice3"),"choice2"),
+      #checkboxInput("in6","CheckName:",TRUE),
+      #br(), # extra empty line
       tags$hr(), # horizontal line
       submitButton("Update") # wait for update button
       
@@ -23,11 +23,11 @@ shinyUI(fluidPage(
     mainPanel(
       tabsetPanel(type="tabs",
                   tabPanel("TabOne",
-                           checkboxInput("in7","CheckName:",TRUE),
+                           #checkboxInput("in7","CheckName:",TRUE),
                            h3(textOutput("out2")), # from in2
                            plotOutput("outplot"),
-                           verbatimTextOutput("summary"),
                            tableOutput("view")
+                           #verbatimTextOutput("summary")
                   ),
                   tabPanel("TabTwo"
                            # Empty
